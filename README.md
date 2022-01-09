fe6b79afe0b6920e7e798c61b63f4026676280f6
Uiseop Eom <ytrtef@gmail.com>
2022-01-10 08:58:24 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MisoSwiftTools
+
+A description of this package.
---
+++ Tests/MisoSwiftToolsTests/MisoSwiftToolsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MisoSwiftTools
+
+final class MisoSwiftToolsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        //XCTAssertEqual(MisoSwiftTools().text, "Hello, World!")
+    }
+}
